cb8657c911e8ae263c735453797c6575a311d957
guilherme <hagiwara.guilherme@gmail.com>
2021-11-19 23:34:33 -0300

    first commit

---
+++ app/src/main/java/com/example/letras/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.letras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        infoPrincipal()
+
+        val textViewNome = findViewById<TextView>(R.id.nome)
+        textViewNome.text = NOME
+
+        val botaoMusicaTraducao = findViewById<Button>(R.id.buttonInfoSecundaria)
+        botaoMusicaTraducao.setOnClickListener {
+            infoSecundaria()
+        }
+
+        val botaoMusicaOriginal = findViewById<Button>(R.id.buttonInfo)
+        botaoMusicaOriginal.setOnClickListener {
+            infoPrincipal()
+        }
+
+    }
+
+    fun infoPrincipal(){
+
+        val textViewMusica = findViewById<TextView>(R.id.texto)
+        textViewMusica.text = INFORMACOES_PRINCIPAIS
+
+    }
+    fun infoSecundaria(){
+
+        val textViewMusica = findViewById<TextView>(R.id.texto)
+        textViewMusica.text = INFORMACOES_SECUNDARIAS
+
+    }
+    companion object {
+        val NOME = "Guilherme Hagiwara"
+        val INFORMACOES_PRINCIPAIS = "Idade: 21\n"+
+                "Sexo: Masculino"
+        val INFORMACOES_SECUNDARIAS = "Gosto de jogar video game\n"+
+                "ouvir musicas e dormir"
+
+    }
+}
